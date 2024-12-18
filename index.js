7aa63711a97251e75049a4be22c609ee3d58e521
MontagneIhsane <ihsane.montagne@gmail.com>
2024-12-18 23:38:07 +0100

    Creation du index.js et ajout de la class Dinosaur

---
+++ index.js
@@ -0,0 +1,9 @@
+class Dinosaur {
+    constructor(name, species, age, diet, status) {
+    this.name = name;        
+    this.species = species;  
+    this.age = age;          
+    this.diet = diet;        
+    this.status = status;    
+    }
+}
