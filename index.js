05d2cdc477d05a6c8ec0fa70feed38b95cfdfdec
MontagneIhsane <ihsane.montagne@gmail.com>
2024-12-18 23:52:56 +0100

    Ajout de la enclos et de ces methodes

--- index.js
+++ index.js
@@ -39,3 +39,39 @@ class Carnivore extends Dinosaur {
     console.log(`${this.name} chasse ${this.preferredPrey}.`);
     }
 }
+
+class Enclosure {
+    constructor(id, size, dinosaurs = [], location, type) {
+        this.id = id;             
+        this.size = size;           
+        this.dinosaurs = dinosaurs; 
+        this.location = location;   
+        this.type = type; 
+    }
+
+    addDinosaur(dinosaur) {
+    this.dinosaurs.push(dinosaur);
+    console.log(`${dinosaur.name} a été ajouté à l'enclos ${this.id}.`);
+    }
+
+    removeDinosaur(dinosaurName) {
+    const index = this.dinosaurs.findIndex(dino => dino.name === dinosaurName);
+    if (index !== -1) {
+        this.dinosaurs.splice(index, 1);
+        console.log(`${dinosaurName} a été retiré de l'enclos ${this.id}.`);
+    } else {
+        console.log(`Le dinosaure ${dinosaurName} n'est pas dans l'enclos ${this.id}.`);
+    }
+    }
+
+    listDinosaurs() {
+    if (this.dinosaurs.length === 0) {
+        console.log(`L'enclos ${this.id} est vide.`);
+    } else {
+        console.log(`Dinosaurs dans l'enclos ${this.id}:`);
+        this.dinosaurs.forEach(dinosaur => {
+        console.log(`- ${dinosaur.name} (${dinosaur.species})`);
+        });
+    }
+    }
+}
