58e18710e4c20ab05063ba1d08d6acd9c4c5316e
MontagneIhsane <ihsane.montagne@gmail.com>
2024-12-19 09:53:50 +0100

    Ajout des exceptions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 class Dinosaur {
     constructor(name, species, age, diet, status) {
+        if (!name || !species || !age || !diet || !status) {
+            throw new Error("Tous les attributs du dinosaure doivent être fournis.");
+        }
     this.name = name;        
     this.species = species;  
     this.age = age;          
@@ -50,28 +53,50 @@ class Enclosure {
     }
 
     addDinosaur(dinosaur) {
-    this.dinosaurs.push(dinosaur);
-    console.log(`${dinosaur.name} a été ajouté à l'enclos ${this.id}.`);
+        try {
+        if (!(dinosaur instanceof Dinosaur)) {
+            throw new Error("L'objet ajouté doit être une instance de Dinosaur.");
+        }
+    
+          if (this.usedSpace + 1 > this.size) {
+            throw new Error(
+            `Espace insuffisant dans l'enclos ${this.id} pour ajouter ${dinosaur.name}.`
+            );
+        }
+    
+        if (!dinosaur.name || !dinosaur.species || !dinosaur.age) {
+            throw new Error(
+            `Impossible d'ajouter un dinosaure : des informations essentielles sont manquantes.`
+            );
+        }
+    
+        this.dinosaurs.push(dinosaur);
+        this.usedSpace += 1; 
+        console.log(`${dinosaur.name} a été ajouté à l'enclos ${this.id}.`);
+        } catch (error) {
+        console.error(error.message);
+        }
     }
-
+    
     removeDinosaur(dinosaurName) {
-    const index = this.dinosaurs.findIndex(dino => dino.name === dinosaurName);
-    if (index !== -1) {
+        const index = this.dinosaurs.findIndex(dino => dino.name === dinosaurName);
+        if (index !== -1) {
         this.dinosaurs.splice(index, 1);
+        this.usedSpace -= 1;
         console.log(`${dinosaurName} a été retiré de l'enclos ${this.id}.`);
-    } else {
+        } else {
         console.log(`Le dinosaure ${dinosaurName} n'est pas dans l'enclos ${this.id}.`);
+        }
     }
-    }
-
+    
     listDinosaurs() {
-    if (this.dinosaurs.length === 0) {
+        if (this.dinosaurs.length === 0) {
         console.log(`L'enclos ${this.id} est vide.`);
-    } else {
+        } else {
         console.log(`Dinosaurs dans l'enclos ${this.id}:`);
         this.dinosaurs.forEach(dinosaur => {
-        console.log(`- ${dinosaur.name} (${dinosaur.species})`);
+            console.log(`- ${dinosaur.name} (${dinosaur.species})`);
         });
+        }
     }
     }
-}
